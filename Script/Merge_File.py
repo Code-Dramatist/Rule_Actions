aba83c28942f9b6ef738ff3a778d29c68239c05d
Code-Dramatist <lu982524190@gmail.com>
2022-08-15 22:19:37 +0800

    bug

--- Script/Merge_File.py
+++ Script/Merge_File.py
@@ -4,9 +4,13 @@ File_Path = ["Direct_Rule","Proxy_Rule","Reject_Rule"]
 for Path in File_Path:
     if not os.path.exists(Path):
         os.system(r"touch {}".format(Path))
-    file_name = os.listdir("./"+Path)
-    if not len(file_name) == 0:
+        print("创建目录{}成功".format(Path))
+    try:
         os.remove("./"+Path+"/"+Path+".rule")
+        print("旧规则移除成功")
+    except FileNotFoundError:
+        print("无旧规则")
+    file_name = os.listdir("./"+Path)
     for file in file_name:
         f = open("./"+Path+"/"+file).read()
         f_merge = open("./"+Path+"/"+Path+".rule","a+")
