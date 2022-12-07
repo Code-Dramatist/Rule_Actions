4d59943af741721f44cf9a3971cea0e0f80ab06d
Code-Dramatist <lu982524190@gmail.com>
2022-12-07 16:31:46 +0800

    去重

--- Script/Merge_File.py
+++ Script/Merge_File.py
@@ -1,6 +1,19 @@
 import os
 File_Path = ["Direct_Rule","Proxy_Rule","Reject_Rule"]
 
+def file_remove_same(input_file, output_file):
+    """
+        针对小文件去重
+    :param input_file: 输入文件
+    :param out_file: 去重后出文件
+    :return:
+    """
+    with open(input_file, 'r', encoding='utf8') as f, open(output_file, 'a', encoding='utf8') as ff:
+        data = [item.strip() for item in f.readlines()]  # 针对最后一行没有换行符，与其他它行重复的情况
+        new_data = list(set(data))
+        ff.writelines([item + '\n' for item in new_data if item])  # 针对去除文件中有多行空行的情况
+
+
 for Path in File_Path:
     if not os.path.exists(Path):
         os.system(r"touch {}".format(Path))
@@ -13,6 +26,9 @@ for Path in File_Path:
     file_name = os.listdir("./"+Path)
     for file in file_name:
         f = open("./"+Path+"/"+file).read()
-        f_merge = open("./"+Path+"/"+Path+".rule","a+")
+        f_merge = open("./"+Path+"/"+Path+"_pre.rule","a+")
         f_merge.write(f)
+        file_remove_same("./"+Path+"/"+Path+"_pre.rule","./"+Path+"/"+Path+".rule")
     f_merge.close()    
+
+
